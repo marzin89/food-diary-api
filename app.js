b73305444e9a8dd99179b21832dc11128483ca95
marco <mazi2001@student.miun.se>
2022-01-14 23:27:33 +0100

    Add access control allow origin header.

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // Möjliggör CORS-anrop
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // Hämtar mongoose
 const mongoose = require('mongoose');
 // URL till databasen
